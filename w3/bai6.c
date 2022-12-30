9a74a442e9fb708cf2756503a4862d0753ffcecb
Tan <ngoctana1k2014@gmail.com>
2022-12-30 13:48:28 +0700

    fix w3 bai 6

--- w3/bai6.c
+++ w3/bai6.c
@@ -3,6 +3,10 @@
 int main() {
     float r;
     scanf("%f", &r);
+    if (r <= 0) {
+        printf("ERROR.\n");
+        return 0;
+    }
     float PI = 3.14159;
     float M = 2 * PI * r;
     float S = PI * r * r;
