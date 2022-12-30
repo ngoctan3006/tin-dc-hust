de8e9bcbb85d017aa0a8d39e86ae2378c9746ea0
Tan <ngoctana1k2014@gmail.com>
2022-12-30 13:11:09 +0700

    fix w3 bai 5

--- w3/bai5.c
+++ w3/bai5.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    float x, y, z;
+    float x, y, z, sum, aver;
     scanf("%f%f%f", &x, &y, &z);
-    float sum = x + y + z;
-    float aver = sum / 3;
+    sum = x + y + z;
+    aver = sum / 3;
     printf("%f\n%f\n", sum, aver);
     return 0;
 }
